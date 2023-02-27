be16197ef018c9f9f75b93fbdeb1a1a8f5ad66b1
Jérémie Valdenaire <jeremie.v@studiometa.fr>
2023-02-27 09:58:32 +0100

    DEV-176 Modifie le modèle User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,7 +15,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'lastname' => $this->faker->lastName(),
+            'firstname' => $this->faker->firstName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
